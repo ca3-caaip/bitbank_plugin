d4223bbfed596f00f9ef09dbb64136f4e740bdcd
watagori <cottongorillla@gmail.com>
2022-05-20 11:27:39 +0900

    feat: mock request using requests_mock

--- tests/test_bitbank_plugin.py
+++ tests/test_bitbank_plugin.py
@@ -1,14 +1,19 @@
 import json
+import os
 from decimal import Decimal
+from pathlib import Path
 from typing import Union
 from unittest.mock import MagicMock
 
 from senkalib.chain.bitbank.bitbank_transaction import BitbankTransaction
+from senkalib.token_original_id_table import TokenOriginalIdTable
 
 from bitbank_plugin.bitbank_plugin import BitbankPlugin
 
 
 class TestBitbankPlugin:
+    TOKEN_ORIGINAL_IDS_URL = "https://raw.githubusercontent.com/ca3-caaip/token_original_id/master/token_original_id.csv"
+
     def test_can_handle_bitbank(self):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 0
@@ -17,13 +22,20 @@ class TestBitbankPlugin:
         chain_type = BitbankPlugin.can_handle(transaction)
         assert chain_type
 
-    def test_get_caajs_buy_fee(self):
+    def test_get_caajs_buy_fee(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 0
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
         assert caajs[2].executed_at == "2022/03/14 11:55:24"
         assert caajs[2].chain == "bitbank"
         assert caajs[2].platform == "bitbank"
@@ -31,20 +43,27 @@ class TestBitbankPlugin:
         assert caajs[2].transaction_id == "1215140489"
         assert caajs[2].type == "lose"
         assert caajs[2].amount == Decimal("71.4924")
-        assert caajs[2].token_symbol == "test_symbol"
+        assert caajs[2].token_symbol == "jpy"
         assert caajs[2].token_original_id == "jpy"
         assert caajs[2].caaj_from == "self"
-        assert caajs[2].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[2].caaj_to == "bitbank"
         assert caajs[2].comment == ""
 
-    def test_get_caajs_sell_fee(self):
+    def test_get_caajs_sell_fee(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 1
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
+
         assert caajs[2].executed_at == "2022/03/14 11:55:24"
         assert caajs[2].chain == "bitbank"
         assert caajs[2].platform == "bitbank"
@@ -52,20 +71,27 @@ class TestBitbankPlugin:
         assert caajs[2].transaction_id == "1215140486"
         assert caajs[2].type == "lose"
         assert caajs[2].amount == Decimal("71.4924")
-        assert caajs[2].token_symbol == "test_symbol"
+        assert caajs[2].token_symbol == "jpy"
         assert caajs[2].token_original_id == "jpy"
         assert caajs[2].caaj_from == "self"
-        assert caajs[2].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[2].caaj_to == "bitbank"
         assert caajs[2].comment == ""
 
-    def test_get_caajs_buy_lose(self):
+    def test_get_caajs_buy_lose(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 0
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
+
         assert caajs[0].executed_at == "2022/03/14 11:55:24"
         assert caajs[0].chain == "bitbank"
         assert caajs[0].platform == "bitbank"
@@ -73,20 +99,27 @@ class TestBitbankPlugin:
         assert caajs[0].transaction_id == "1215140489"
         assert caajs[0].type == "lose"
         assert caajs[0].amount == Decimal("59577.0126674")
-        assert caajs[0].token_symbol == "test_symbol"
+        assert caajs[0].token_symbol == "jpy"
         assert caajs[0].token_original_id == "jpy"
         assert caajs[0].caaj_from == "self"
-        assert caajs[0].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[0].caaj_to == "bitbank"
         assert caajs[0].comment == ""
 
-    def test_get_caajs_buy_get(self):
+    def test_get_caajs_buy_get(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 0
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
+
         assert caajs[1].executed_at == "2022/03/14 11:55:24"
         assert caajs[1].chain == "bitbank"
         assert caajs[1].platform == "bitbank"
@@ -94,20 +127,27 @@ class TestBitbankPlugin:
         assert caajs[1].transaction_id == "1215140489"
         assert caajs[1].type == "get"
         assert caajs[1].amount == Decimal("537.8006")
-        assert caajs[1].token_symbol == "test_symbol"
+        assert caajs[1].token_symbol == "mona"
         assert caajs[1].token_original_id == "mona"
         assert caajs[1].caaj_from == "self"
-        assert caajs[1].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[1].caaj_to == "bitbank"
         assert caajs[1].comment == ""
 
-    def test_get_caajs_sell_lose(self):
+    def test_get_caajs_sell_lose(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 1
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
+
         assert caajs[0].executed_at == "2022/03/14 11:55:24"
         assert caajs[0].chain == "bitbank"
         assert caajs[0].platform == "bitbank"
@@ -115,20 +155,27 @@ class TestBitbankPlugin:
         assert caajs[0].transaction_id == "1215140486"
         assert caajs[0].type == "lose"
         assert caajs[0].amount == Decimal("537.8006")
-        assert caajs[0].token_symbol == "test_symbol"
+        assert caajs[0].token_symbol == "mona"
         assert caajs[0].token_original_id == "mona"
         assert caajs[0].caaj_from == "self"
-        assert caajs[0].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[0].caaj_to == "bitbank"
         assert caajs[0].comment == ""
 
-    def test_get_caajs_sell_get(self):
+    def test_get_caajs_sell_get(self, requests_mock):
         test_data = TestBitbankPlugin._get_test_data(
             "tests/data/bitbank_sample_with_data_type.json", 1
         )
         transaction = BitbankTransaction(test_data)
-        mock = TestBitbankPlugin._get_token_table_mock()
-        caajs = BitbankPlugin.get_caajs(transaction, mock)
+        data = Path(
+            "%s/data/token_original_id.csv" % os.path.dirname(__file__)
+        ).read_text()
+        requests_mock.get(TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL, text=data)
+        token_original_ids = TokenOriginalIdTable(
+            TestBitbankPlugin.TOKEN_ORIGINAL_IDS_URL
+        )
+
+        caajs = BitbankPlugin.get_caajs(transaction, token_original_ids)
+
         assert caajs[1].executed_at == "2022/03/14 11:55:24"
         assert caajs[1].chain == "bitbank"
         assert caajs[1].platform == "bitbank"
@@ -136,10 +183,9 @@ class TestBitbankPlugin:
         assert caajs[1].transaction_id == "1215140486"
         assert caajs[1].type == "get"
         assert caajs[1].amount == Decimal("59577.0126674")
-        assert caajs[1].token_symbol == "test_symbol"
+        assert caajs[1].token_symbol == "jpy"
         assert caajs[1].token_original_id == "jpy"
         assert caajs[1].caaj_from == "self"
-        assert caajs[1].token_symbol_uuid == "3a2570c5-15c4-2860-52a8-bff14f27a236"
         assert caajs[1].caaj_to == "bitbank"
         assert caajs[1].comment == ""
 
